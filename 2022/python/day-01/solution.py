7f4e94561705f3198176e058a5c19698d6e41ae8
thebooleanguy <tharindus0601@gmail.com>
2023-11-04 17:28:58 +0530

    Completed day 01

--- 2022/python/day-01/solution.py
+++ 2022/python/day-01/solution.py
@@ -1,10 +1,32 @@
-# Read file line by line into array
-# Add the calories to another array, seperating by blank element
-# Find max
+# Program to Read File and do some Calculations using Arrays, then Sort them
 
-with open('test.txt', 'r') as test:
-    # For loop and save into array or list
-    for line in test:
-        # test_line = test.readline()
-        # print(test_line, end='')
-        print(line, end='')
+lines = []
+# Open file for reading
+with open('input.txt', 'r') as file:
+    # For loop to save lines into a list
+    for line in file:
+        lines.append(line)
+
+# Else cant access 0th element because no element
+totals = [0]
+# For loop to add the totals to another list, seperating by newline
+newline_count = 0
+for line in lines:
+    if line == '\n':
+        newline_count += 1
+        # Else no element to access and do the addition
+        totals.append(0)
+        continue
+    totals[newline_count] += int(line)
+
+max = [0, 0, 0]
+# Repeat 3 times to get top 3 maxes
+for i in range(3):
+    # For loop to get max
+    for total in totals:
+        if total > max[i]:
+            max[i] = total
+    # Remove max from totals list
+    totals.remove(max[i])
+
+print(max[0] + max[1] + max[2])
