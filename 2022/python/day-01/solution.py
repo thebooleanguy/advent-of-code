f92217e025915e3529b9ca33bb4786e67828067f
thebooleanguy <tharindus0601@gmail.com>
2023-11-04 13:29:09 +0530

    Categorize by language

---
+++ 2022/python/day-01/solution.py
@@ -0,0 +1,10 @@
+# Read file line by line into array
+# Add the calories to another array, seperating by blank element
+# Find max
+
+with open('test.txt', 'r') as test:
+    # For loop and save into array or list
+    for line in test:
+        # test_line = test.readline()
+        # print(test_line, end='')
+        print(line, end='')
